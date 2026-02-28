5e95c36cc866c045a857f5115a8a64cc697a9b29
Max <barreiro.fontela.max@gmail.com>
2026-02-28 17:36:53 +0200

    Initial Commit

---
+++ CD/CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDApp.swift
+//  CD
+//
+//  Created by MaxAdmin on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
